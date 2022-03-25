4998bbeef087c08b0d33b278d80334172a4e2df8
ederxx <93275824+ederxx@users.noreply.github.com>
2022-03-25 13:55:36 -0300

    ljgbihv

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -7,7 +7,14 @@ import { Button } from '@material-ui/core';
 function SignIn() {
     function signInWithGoogle() {
         const provider = new firebase.auth.GoogleAuthProvider();
-        auth.signInWithPopup(provider)
+        provider.addScope('profile');
+        provider.addScope('email');
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+         // This gives you a Google Access Token.
+         var token = result.credential.accessToken;
+         // The signed-in user info.
+         var user = result.user;
+        });
             }
             return (
                 <div className="signIn">
